16387bf4d5539ab2bbb93b9967eb942474d5cd40
akani28 <nietocevallos61@gmail.com>
2023-11-03 05:45:02 -0500

    update

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const PokemonDetail = () => {
-  return (
-    <div>PokemonDetail</div>
-  )
-}
+  const [pokemon, setPokemon] = useState(null);
+  const { pokemonId } = useParams();
+  console.log(pokemonId);
 
-export default PokemonDetail
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(({ data }) => {
+        setPokemon(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <article>
+      <main>
+        <header>
+          <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+        </header>
+        <h3>{pokemon?.id}</h3>
+        <h2>{pokemon?.name}</h2>
+        <section>
+          <h3>Stats</h3>
+          <ul>
+            {
+              pokemon?.stats.map((stats)=><li>
+                <div>
+                  <h5></h5>
+                  <span></span>
+                </div>
+              </li>)
+            }
+          </ul>
+        </section>
+      </main>
+    </article>
+  );
+};
+
+export default PokemonDetail;
