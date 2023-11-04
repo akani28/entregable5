e301829e24d0fa171c6b43f4cdca75dbe0c01749
akani28 <nietocevallos61@gmail.com>
2023-11-04 04:03:50 -0500

    update

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -5,6 +5,13 @@ import { useParams } from "react-router-dom";
 const PokemonDetail = () => {
   const [pokemon, setPokemon] = useState(null);
   const { pokemonId } = useParams();
+
+  const getPercentStat=(statValue)=>{
+    const MAX_STAT_VALUE = 255
+    const percentStat = ((100 * statValue)/MAX_STAT_VALUE).toFixed(1)
+    return `${percentStat}%`
+
+  }
   console.log(pokemonId);
 
   useEffect(() => {
@@ -19,28 +26,34 @@ const PokemonDetail = () => {
       });
   }, []);
   return (
-    <article>
-      <main>
+    <main className="py-10 px-2">
+      <article>
         <header>
-          <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+          <img
+            src={pokemon?.sprites.other["official-artwork"].front_default}
+            alt=""
+          />
         </header>
         <h3>{pokemon?.id}</h3>
         <h2>{pokemon?.name}</h2>
         <section>
           <h3>Stats</h3>
           <ul>
-            {
-              pokemon?.stats.map((stats)=><li>
-                <div>
-                  <h5></h5>
-                  <span></span>
+            {pokemon?.stats.map((stat) => (
+              <li key={stat.stat.name}>
+                <div className="flex justify-between items-center">
+                  <h5>{stat.stat.name}</h5>
+                  <span>{stat.base_stat}/255</span>
                 </div>
-              </li>)
-            }
+                <div className="bg-slate-200 rounded-md h-6 overflow-hidden">
+                  <div style={{width:getPercentStat(stat.base_stat)}} className="bg-orange-400 h-full"></div>
+                </div>
+              </li>
+            ))}
           </ul>
         </section>
-      </main>
-    </article>
+      </article>
+    </main>
   );
 };
 
